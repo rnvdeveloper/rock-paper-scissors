9dfde6fa134c48c4365ed791873599e026fa1290
Diego Vieira <diegovieira.dvf@gmail.com>
2023-03-22 21:35:13 -0300

    Create a variable to store function return (getComputerChoice())

--- config.js
+++ config.js
@@ -4,4 +4,6 @@ let randomNumber = Math.floor(Math.random() * choices.length);
 
 function getComputerChoice() {
     return choices[randomNumber];
-} 
+}
+
+let computerSelection = getComputerChoice();
