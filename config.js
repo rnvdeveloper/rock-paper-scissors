d2424ea05a3040a5f70585e1ad41f253bacc9cd0
Diego Vieira <diegovieira.dvf@gmail.com>
2023-03-23 16:13:54 -0300

    Create a function that plays a single round of Rock Paper Scissors and returns who wins the match

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-let choices = ["Rock", "Paper", "Scissors"];
+let choices = ["rock", "paper", "scissors"];
 
 let randomNumber = Math.floor(Math.random() * choices.length);
 
@@ -6,8 +6,10 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
-let computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice(); // Podia jogar a funcao direto em playRound()?
 let controlInput = true;
+
+// This can be a function?
 let playerSelection = prompt("Rock, Paper or Scissors?", "").toLowerCase();
 
 
@@ -22,4 +24,34 @@ while (controlInput) {
     }
 }
 
+console.log(computerSelection);
 console.log(playerSelection);
+
+function playRound(computerSelection, playerSelection) {
+    computerSelection = computerSelection;
+    playerSelection = playerSelection;
+
+    if (computerSelection == playerSelection) {
+        return "It's a tie!";
+    }
+    else if (computerSelection === "rock" && playerSelection === "scissors") {
+        return "You Lose! Rock beats Scissors";
+    }
+    else if (computerSelection === "rock" && playerSelection === "paper") {
+        return "You Win! Paper beats Rock";
+    }
+    else if (computerSelection === "paper" && playerSelection === "rock") {
+        return "You Lose! Paper beats Rock";
+    }
+    else if (computerSelection === "paper" && playerSelection === "scissors") {
+        return "You Win! Scissors beats Paper";
+    }
+    else if (computerSelection === "scissors" && playerSelection === "paper") {
+        return "You Lose! Scissors beats Paper";
+    }
+    else if (computerSelection === "scissors" && playerSelection === "rock") {
+        return "You Win! Rock beats Scissors";
+    }
+}
+
+console.log(playRound(computerSelection, playerSelection));
