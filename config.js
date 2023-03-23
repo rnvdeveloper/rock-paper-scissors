be165240d55b07623751ac6c7f61769c77f6f4cc
Diego Vieira <diegovieira.dvf@gmail.com>
2023-03-22 23:51:05 -0300

    Create variable to catch and store the user choice , make case-insensitive and validate input

--- config.js
+++ config.js
@@ -7,3 +7,19 @@ function getComputerChoice() {
 }
 
 let computerSelection = getComputerChoice();
+let controlInput = true;
+let playerSelection = prompt("Rock, Paper or Scissors?", "").toLowerCase();
+
+
+while (controlInput) {
+    if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+
+        playerSelection = prompt("Rock, Paper or Scissors?", "").toLowerCase();
+    }
+    else {
+        playerSelection = playerSelection;
+        controlInput = false;
+    }
+}
+
+console.log(playerSelection);
