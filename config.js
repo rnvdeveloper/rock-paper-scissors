8e5f790e9f4b35eac7ed7bb03b16ea250eb5c721
Diego Vieira <diegovieira.dvf@gmail.com>
2023-03-27 11:39:26 -0300

    Add a score in playRound() function and a message in the function game() that reports a winner or loser at the end

--- config.js
+++ config.js
@@ -22,6 +22,8 @@ function getPlayerChoice() {
     return playerInput;
 }
 
+let computerScore = 0;
+let userScore = 0;
 
 function playRound() {
     let computerSelection = getComputerChoice();
@@ -31,30 +33,43 @@ function playRound() {
         return "It's a tie!";
     }
     else if (computerSelection === "rock" && playerSelection === "scissors") { // tirar elses
-        return "You Lose! Rock beats Scissors";
+        computerScore++;
+        return "You Lose! Rock beats Scissors"; 
     }
     else if (computerSelection === "rock" && playerSelection === "paper") {
+        userScore++;
         return "You Win! Paper beats Rock";
     }
     else if (computerSelection === "paper" && playerSelection === "rock") {
+        computerScore++;
         return "You Lose! Paper beats Rock";
     }
     else if (computerSelection === "paper" && playerSelection === "scissors") {
+        userScore++;
         return "You Win! Scissors beats Paper";
     }
     else if (computerSelection === "scissors" && playerSelection === "paper") {
+        computerScore++;
         return "You Lose! Scissors beats Paper";
     }
     else if (computerSelection === "scissors" && playerSelection === "rock") {
+        userScore++;
         return "You Win! Rock beats Scissors";
     }
 }
 
 function game() {
-    for (let index = 5; index > 1; index--) {
+    for (let index = 5; index >= 1; index--) {
         console.log(playRound());
     }
-    return playRound();
+
+    if (computerScore > userScore) {
+        console.log("You Lose!");
+        
+    }
+    else {
+        console.log("You are the winner!");
+    }
 }
 
 game();
